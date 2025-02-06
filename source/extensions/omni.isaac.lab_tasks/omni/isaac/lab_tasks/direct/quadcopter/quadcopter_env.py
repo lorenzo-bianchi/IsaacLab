d67e6d497ceb719f7fa611fea651500049e89b53
Lorenzo Bianchi <lnz.bnc@gmail.com>
2025-02-06 12:11:18 -0500

    test: update rewards

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/quadcopter/quadcopter_env.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/quadcopter/quadcopter_env.py
@@ -291,8 +291,8 @@ class QuadcopterEnv(DirectRLEnv):
         new_point = torch.logical_and(close_to_goal, change_setpoint)
 
         rewards = {
-            # "lin_vel": lin_vel * self.cfg.lin_vel_reward_scale * self.step_dt,
-            # "ang_vel": ang_vel * self.cfg.ang_vel_reward_scale * self.step_dt,
+            "lin_vel": lin_vel * self.cfg.lin_vel_reward_scale * self.step_dt,
+            "ang_vel": ang_vel * self.cfg.ang_vel_reward_scale * self.step_dt,
 
             "approaching_to_goal": approaching * self.cfg.approaching_goal_reward_scale * self.step_dt,
             "convergence_to_goal": convergence * self.cfg.convergence_goal_reward_scale * self.step_dt,
