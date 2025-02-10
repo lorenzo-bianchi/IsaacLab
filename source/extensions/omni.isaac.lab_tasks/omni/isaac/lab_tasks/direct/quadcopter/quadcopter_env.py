ba180aa13d1523177aa55be93f81c13775375e93
Lorenzo Bianchi <lnz.bnc@gmail.com>
2025-02-10 12:30:56 -0500

    test: remove most rewards

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/quadcopter/quadcopter_env.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/quadcopter/quadcopter_env.py
@@ -108,7 +108,7 @@ class QuadcopterEnvCfg(DirectRLEnvCfg):
     # reward scales
     lin_vel_reward_scale = -0.2            # rsl_rl
     ang_vel_reward_scale = -0.05
-    approaching_goal_reward_scale = 500.0
+    approaching_goal_reward_scale = 1000.0
     convergence_goal_reward_scale = 1000.0
     yaw_reward_scale =  300.0
     new_goal_reward_scale = 100.0
