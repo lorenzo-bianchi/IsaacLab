9c43b93967913b44f9b3e8dba26f615334cc48ab
Lorenzo Bianchi <lnz.bnc@gmail.com>
2025-02-19 19:11:58 -0500

    test: remove yaw reward

--- source/standalone/workflows/rsl_rl/train.py
+++ source/standalone/workflows/rsl_rl/train.py
@@ -101,7 +101,7 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     ang_vel_reward_scale = -0.05
     approaching_goal_reward_scale = 900.0
     convergence_goal_reward_scale = 600.0
-    yaw_reward_scale = 300.0
+    yaw_reward_scale = 0.0
     new_goal_reward_scale = 100.0
     cmd_smoothness_reward_scale = -1.5
     cmd_body_rates_reward_scale = -0.3
