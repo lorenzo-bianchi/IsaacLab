1d8c9171dd3a7bc6a4c92642921fe5510dd86676
Lorenzo Bianchi <lnz.bnc@gmail.com>
2025-02-24 20:09:14 -0500

    test: update rewards

--- source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/quadcopter/quadcopter_env.py
+++ source/extensions/omni.isaac.lab_tasks/omni/isaac/lab_tasks/direct/quadcopter/quadcopter_env.py
@@ -180,7 +180,7 @@ class QuadcopterEnvCfg(DirectRLEnvCfg):
     # Parameters from train.py or play.py
     use_simple_model = None
     prob_change = 0.5
-    proximity_threshold = 0.1
+    proximity_threshold = 0.2
     velocity_threshold = 100.0
     wait_time_s = 1.0
     rewards = {}
