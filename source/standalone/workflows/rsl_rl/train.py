fbaa0523f7e6a0a038eff01aa42fb41c6ba4b112
Lorenzo Bianchi <lnz.bnc@gmail.com>
2025-02-26 20:43:46 -0500

    test: update rewards

--- source/standalone/workflows/rsl_rl/train.py
+++ source/standalone/workflows/rsl_rl/train.py
@@ -100,7 +100,7 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     lin_vel_reward_scale = -0.4
     ang_vel_reward_scale = -0.05
     approaching_goal_reward_scale = 2700.0
-    convergence_goal_reward_scale = 600.0
+    convergence_goal_reward_scale = 800.0
     yaw_reward_scale = 0.0
     new_goal_reward_scale = 500.0
     cmd_body_rates_reward_scale = 0.0
